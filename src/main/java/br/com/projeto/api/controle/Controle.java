7220820b6b236d282af5c7231f0104ae688e7f7c
GuilhermeSouzaViana <vianagui7744@gmail.com>
2023-12-01 18:07:26 -0300

    first commit

---
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,48 @@
+package br.com.projeto.api.controle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Cliente;
+import br.com.projeto.api.repositorio.Repositorio;
+
+@RestController
+public class Controle {
+
+
+@Autowired
+private Repositorio acao;    
+    
+@GetMapping("/")
+public String teste(){
+    return "hello angular";
+}
+
+@PostMapping("/cadastro")
+public Cliente cadastrar(@RequestBody Cliente c){
+    return acao.save(c);
+}
+
+
+@GetMapping("/listar")
+public Iterable<Cliente>selecionar(){
+    return acao.findAll();
+}
+
+@PutMapping("/editar")
+public Cliente editar(@RequestBody Cliente c){
+    return acao.save(c);
+}
+
+@DeleteMapping("/remover/{codigo}")
+public void remover(@PathVariable Long codigo){
+     acao.deleteById(codigo);
+}
+
+}
